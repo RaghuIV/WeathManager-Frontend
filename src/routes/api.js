7a44f2bb5bfe7e6369e7320d10e26e77db76395b
Raghu <raghunathsingh2410@gmail.com>
2025-08-08 21:22:51 +0530

    UPdated base url for api calls

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/portfolio/';
+const BASE_URL = 'https://weathmanager-backend.onrender.com';
 
 export async function getHoldings() {
   const res = await fetch(`${BASE_URL}holdings`);
