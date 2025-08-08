4e366388b8ecc985f104076364979c7ff0b4c819
Raghu <raghunathsingh2410@gmail.com>
2025-08-08 21:34:40 +0530

    updated url endpoints

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://weathmanager-backend.onrender.com/';
+const BASE_URL = 'https://weathmanager-backend.onrender.com/api/portfolio/';
 
 export async function getHoldings() {
   const res = await fetch(`${BASE_URL}holdings`);
